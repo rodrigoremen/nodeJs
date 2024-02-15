83e365ee9e334ecb90dacaf739ffa28774d4862c
Rodrigo Dorantes <rodrigoivan234@gmail.com>
2024-02-15 12:21:56 -0600

    Update function to handle edge cases

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import { log } from 'console';
+import express from 'express';
+import { dirname, join} from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
+app.set('views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('index'));
+
+app.listen(3000, () => {
+	console.log('Server is running on port 3000');
+});
